b18e907f09d19dcb5d0ecb4d85fef5d969a79173
Juan Fran Muñoz Martin <juan0fran@Macbook-JuanFran-2.local>
2016-09-07 08:09:06 +0200

    picc1101 updated

--- src/picc1101/radio.c
+++ src/picc1101/radio.c
@@ -92,7 +92,7 @@ static void     get_chanbw_words(float bw, radio_parms_t *radio_parms);
 static void     get_rate_words(arguments_t *arguments, radio_parms_t *radio_parms);
 static void     wait_for_state(spi_parms_t *spi_parms, ccxxx0_state_t state, uint32_t timeout);
 static void     print_received_packet(int verbose_min);
-static void     radio_send_block(spi_parms_t *spi_parms, uint8_t block_countdown);
+static void     radio_send_block(spi_parms_t *spi_parms);
 static uint8_t  radio_receive_block(spi_parms_t *spi_parms, arguments_t *arguments, uint8_t *block, uint32_t *size, uint8_t *crc);
 static uint8_t  crc_check(uint8_t *block);
 
