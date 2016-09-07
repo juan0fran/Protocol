cdf8f86eaa1e776788a69b1f86b0e50769898ea4
Juan Fran Muñoz Martin <juan0fran@Macbook-JuanFran-2.local>
2016-09-07 09:03:47 +0200

    picc1101 updated

--- src/protocol/StopAndWait.c
+++ src/protocol/StopAndWait.c
@@ -314,6 +314,7 @@ ErrorHandler StopAndWait(Control * c, Status * s){
 				/* Last link updated, round trip time must be updated */
 				/* Every packet sent c->timeout is set */
 				c->round_trip_time = (millitime() - c->timeout) + 2 * c->piggy_time;
+				printf("Rount trip time updated to: %d\n", c->round_trip_time);
 				c->last_link = millitime();
 
 				/* A new packet (sent from other station) has been received while witing for ACK */
