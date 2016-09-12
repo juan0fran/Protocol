3795668663ec6b5d68660c75bc843680955d089e
3cat2 <cubecat.vcn@gmail.com>
2016-09-12 17:25:27 +0200

    Seems to be ok now

--- src/main.c
+++ src/main.c
@@ -301,8 +301,8 @@ int protocol_routine(char * sock_data_phy, char * sock_data_net, char * sock_dat
 		/* Ping link time is 10 seconds */
 		control.ping_link_time = 60000;
 		control.piggy_time = 10;
-		control.byte_round_trip_time = 1500;
-		control.packet_timeout_time = 1500; /* ms */ /* The channel has a delay of 10 ms, so 100 ms per timeout as an example */
+		control.byte_round_trip_time = 500;
+		control.packet_timeout_time = 500; /* ms */ /* The channel has a delay of 10 ms, so 100 ms per timeout as an example */
 		control.round_trip_time = control.packet_timeout_time;
 		control.death_link_time = 60000; /* in ms */ /* after 60 seconds without handshake, test again */
 		log_message(LOG_INFO, "The two socket are initialised\n");
