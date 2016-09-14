e20898b7259788614a8371b9a0fee9438d11d1da
3cat2 <cubecat.vcn@gmail.com>
2016-09-14 08:23:42 +0200

    we need a random back off

--- src/picc1101/kiss.c
+++ src/picc1101/kiss.c
@@ -335,13 +335,14 @@ void kiss_run(serial_t *serial_parms, spi_parms_t *spi_parms, arguments_t *argum
 
             radio_wait_free();            // Make sure no radio operation is in progress
             /* This should mean, no packet being received */
-            radio_turn_idle(spi_parms);   // Inhibit radio operations (should be superfluous since both Tx and Rx turn to IDLE after a packet has been processed)
-            radio_flush_fifos(spi_parms); // Flush result of any Rx activity
-
+            
             verbprintf(2, "%d bytes to send\n", tx_count);
             /* I send the radio packet */
             /* Before sending the packet -> random usleep */
             radio_wait_a_bit(rand()%255);
+            radio_wait_free();            // Make sure no radio operation is in progress
+            radio_turn_idle(spi_parms);   // Inhibit radio operations (should be superfluous since both Tx and Rx turn to IDLE after a packet has been processed)
+            radio_flush_fifos(spi_parms); // Flush result of any Rx activity
             radio_send_packet(spi_parms, arguments, tx_buffer, tx_count);
 
             /* Then put the radio to RX again */
